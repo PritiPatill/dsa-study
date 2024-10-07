28c249f84cd267e786bd390cc6d5e2ae3a493d3e
Priti.Patil <pritipatil@Pritis-MacBook-Pro.local>
2024-10-07 21:56:26 +0530

    prime no modifiled

--- prime_no.js
+++ prime_no.js
@@ -1,3 +1,30 @@
+/*
+Problem statement  - For a given number n check if it is prime or not. A prime number is a number which is only divisible by 1 and itself.
+*/
+
+
+const isPrime = (n) => {
+    let count = 0;
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+        count++;
+      }
+    }
+  
+    if (count == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
+//   Time complexity = O(n)
+
+
+// optimal solution
+
 class Solution {
 
     isPrime(n){
